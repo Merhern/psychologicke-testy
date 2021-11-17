11a82ff765cfde80314663e95d16347868ae1acd
Merhern <67610137+Merhern@users.noreply.github.com>
2021-11-17 13:17:08 +0100

    Eysenc's personality questionare v1

---
+++ EPG.js
@@ -0,0 +1,903 @@
+var otazky = [
+{
+    "otazka":"Do you have many different hobbies?",
+        "efekt":{
+            "P":0,
+            "E":1,
+            "N":0,
+            "L":0
+        }
+},
+{
+    "otazka":"Do you stop to think things over before doing anything?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Does your mood often go up and down?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever taken the praise for something you knew someone else had really done?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you take much notice of what people think?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you a talkative person?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+}
+{
+    "otazka":"Would being in debt worry you?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you ever feel ‘just miserable’ for no reason?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you give money to charities?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Were you ever greedy by helping yourself to more than your share of anything?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Are you rather lively? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Would it upset you a lot to see a child or an animal suffer?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you often worry about things you should not have done or said? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you dislike people who don’t know how to behave themselves?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"If you say you will do something, do you always keep your promise no matter how inconvenient it might be? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Can you usually let yourself go and enjoy yourself at a lively party?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you an irritable person? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Should people always respect the law?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever blamed someone for doing something you knew was really your fault? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you enjoy meeting new people?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are good manners very important?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are your feelings easily hurt?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Are all your habits good and desirable ones?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Do you tend to keep in the background on social occasions? ",
+    "efekt":{
+        "P":0,
+        "E":-1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Would you take drugs which may have strange or dangerous effects?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you often feel ‘fed-up’?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever taken anything (even a pin or button) that belonged to someone else?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you like going out a lot?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you prefer to go your own way rather than act by the rules?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you enjoy hurting people you love?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you often troubled about feelings of guilt?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you sometimes talk about things you know nothing about? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you prefer reading to meeting people?",
+    "efekt":{
+        "P":0,
+        "E":-1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you have enemies who want to harm you?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Would you call yourself a nervous person? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you have many friends? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you enjoy practical jokes that can sometimes really hurt people?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you a worrier? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"As a child did you do as you were told immediately and without grumbling?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Would you call yourself happy-go-lucky? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do good manners and cleanliness matter much to you? ",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you often gone against your parents’ wishes?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you worry about awful things that might happen?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever broken or lost something belonging to someone else? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Do you usually take the initiative in making new friends?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Would you call yourself tense or ‘highly-strung’?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you mostly quiet when you are with other people?",
+    "efekt":{
+        "P":0,
+        "E":-1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you think marriage is old-fashioned and should be done away with? ",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you sometimes boast a little?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Are you more easy-going about right and wrong than most people?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Can you easily get some life into a rather dull party?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you worry about your health?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever said anything bad or nasty about anyone?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Do you enjoy co-operating with others?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you like telling jokes and funny stories to your friends?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do most things taste the same to you?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"As a child were you ever cheeky to your parents? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you like mixing with people?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Does it worry you if you know there are mistakes in your work? ",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you suffer from sleeplessness?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have people said that you sometimes act too rashly? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you always wash before a meal?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Do you nearly always have a ‘ready answer’ when people talk to you? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you like to arrive at appointments in plenty of time?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you often felt listless and tired for no reason?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever cheated at a game?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you like doing things in which you have to act quickly?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Is (or was) your mother a good woman? ",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you often make decisions on the spur of the moment?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you often feel life is very dull? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever taken advantage of someone?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you often take on more activities than you have time for?",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are there several people who keep trying to avoid you? ",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you worry a lot about your looks?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you think people spend too much time safeguarding their future with savings and insurance?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever wished that you were dead? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Would you dodge paying taxes if you were sure you could never be found out? ",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Can you get a party going? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you try not to be rude to people?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you worry too long after an embarrassing experience?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you generally ‘look before you leap’?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever insisted on having your own way?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you suffer from ‘nerves’?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you often feel lonely?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Can you on the whole trust people to tell the truth? ",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you always practice what you preach?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Are you easily hurt when people find fault with you at the work you do?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Is it better to follow society’s rules than go your own way? ",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Have you ever been late for an appointment or work?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+},
+{
+    "otazka":"Do you like plenty of bustle and excitement around you? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Would you like other people to be afraid of you? ",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},    
+{
+    "otazka":"Are you sometimes bubbling over with energy and sometimes very sluggish?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you sometimes put off until tomorrow what you ought to do today?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":-1
+    }
+}, 
+{
+    "otazka":"Do other people think of you as being very lively? ",
+    "efekt":{
+        "P":0,
+        "E":1,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do people tell you a lot of lies?",
+    "efekt":{
+        "P":1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Do you believe one has special duties to one’s family?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }
+},
+{
+    "otazka":"Are you touchy about some things?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    } 
+},
+{
+    "otazka":"Are you always willing to admit it when you have made a mistake?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":0,
+        "L":1
+    }
+},
+{
+    "otazka":"Would you feel very sorry for an animal caught in a trap?",
+    "efekt":{
+        "P":-1,
+        "E":0,
+        "N":0,
+        "L":0
+    }    
+},
+{
+    "otazka":"When your temper rises, do you find it difficult to control?",
+    "efekt":{
+        "P":0,
+        "E":0,
+        "N":1,
+        "L":0
+    }
+}    
+
+]
